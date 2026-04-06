925c00d9a807a2fa8f6e0797f0feee3d42c07568
NikoBK <112914618+NikoBK@users.noreply.github.com>
2026-04-07 00:11:47 +0200

    player controls

--- core/src/main/java/com/nikobk/javadung/Main.java
+++ core/src/main/java/com/nikobk/javadung/Main.java
@@ -1,6 +1,8 @@
 package com.nikobk.javadung;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -11,6 +13,10 @@ public class Main extends ApplicationAdapter {
     private SpriteBatch batch;
     private Texture image;
     private BitmapFont font;
+    
+    // Player props
+    float x = 100;
+    float y = 100;
 
     @Override
     public void create() {
@@ -22,9 +28,14 @@ public class Main extends ApplicationAdapter {
     @Override
     public void render() {
         ScreenUtils.clear(0, 0, 0, 1);
+        
+        if (Gdx.input.isKeyPressed(Input.Keys.W)) y += 200 * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.S)) y -= 200 * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.A)) x -= 200 * Gdx.graphics.getDeltaTime();
+        if (Gdx.input.isKeyPressed(Input.Keys.D)) x += 200 * Gdx.graphics.getDeltaTime();
 
         batch.begin();
-        font.draw(batch, "Hello world", 100, 150);
+        font.draw(batch, "Player", x, y);
         batch.end();
     }
 
